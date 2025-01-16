21cf536281ab4d8609d40c63d8f0eac5491b3508
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2025-01-15 23:58:39 -0500

    updated text to current

--- src/Explainers/LTS.jsx
+++ src/Explainers/LTS.jsx
@@ -4,12 +4,72 @@ console.log('Explainer/LTS loaded')
 
 const LTS = ({}) => {
     return(<div className="content">
-    <h1>[Under Construction] What is this map?</h1>
-        <p>What exactly we want to say and how to format it needs to be figured out. Below isn't a well thought out outline yet.</p>
-        <h2>Biking Stress</h2>
-            <p>Explain what LTS is non-technical terms</p>
+    <h1>Getting Started with this map</h1>
+        <h2>What is this map?</h2>
         <h2>How to use this map</h2>
-            <p>Give some </p>
+        <h2>What if you disagree with a street segment rating?</h2>
+            <p>
+            If a street you ride on regularly feels more or less comfortable than how we have rated it, you are probably right! 
+            We use OSM for our data and it is very incomplete, forcing us to make some assumptions, meaning we are probably wrong 
+            in some places.You can learn to fix the data yourself here [link: improve map], or fill out this form with what you 
+            know and the BCU Labs team will bike there ourselves and fix the data.
+            </p>
+    <h1>A deep dive into Level of Traffic Stress</h1>
+        <p>
+        Our Stress Map is based on a heuristic concept called Level of Traffic Stress (LTS). This has been developed in part by 
+        Prof. Peter Furth of Northeastern University and is commonly used by planning and engineering professionals. Our 
+        implementation attempts to calculate the most current version, v2.2. This version uses many factors of the street design 
+        to categorize streets. Unfortunately, our primary data source, OSM, does not have all of the necessary data tagged for 
+        each street segment. Engineers could manually collect data for individual street improvement projects to accurately rate 
+        a street segment, but to achieve a regional map, we make educated assumptions on what the street looks like. If you see 
+        something wrong, we wrote up how to fix the data here[link: improve map].
+        </p>
+        
+    <h1>Want to help improve this map?</h1>
+        <p>
+        If you are interested in the type of projects we are working on, join us at BCU Labs! [link] Any and all levels of 
+        experience are welcomed. We are always looking for new volunteers that are interested in data-backed analysis and storytelling. 
+        <br />
+        Check out what we are working on in GitHub. [link]
+        </p>
+        <h2>Improve OpenStreetMap</h2>
+            <p>
+            OpenStreetMap (OSM) is the data source for the calculations used to rate each segment. This is an incredible resource, much 
+            like Wikipedia, that is volunteer edited to add details and keep the map current to changes (e.g. new bike lanes being 
+            installed). By editing the map directly, you will make the Stress Map more accurate, and you will improve the baseline 
+            data used in most of your favorite navigation apps, including the BlueBikes app, Strava, RideWithGPS, GeoVelo, Pointz, 
+            and many more.
+            </p>
+            <h3>Recommended Map Editing Tools</h3>
+                <p>
+                These are some of the tools that the BCU Labs team has experience with to make improvements to OpenStreetMap. 
+                There are numerous other tools if you find improving the map a fun hobby, with different strengths for different 
+                types of editing.
+                </p>
+                <h4>StreetComplete (Android only, beginner friendly)</h4>
+                    <p>
+                    https://streetcomplete.app/
+                    </p>
+                <h4>OpenStreetMap.org</h4>
+                    <p>
+                    https://www.openstreetmap.org/
+                    </p>
+                <h4>MapComplete</h4>
+                    <p>
+                    
+                    </p>
+                <h4>OverpassTurbo</h4>
+                    <p>
+                    https://overpass-turbo.eu/ 
+                    </p>
+                <h4>JOSM</h4>
+                    <p>
+                    
+                    </p>
+                <h4>Vespucci</h4>
+                    <p>
+                    
+                    </p>
     </div>
     )
 }
--- src/Explainers/Labs.jsx
+++ src/Explainers/Labs.jsx
@@ -4,12 +4,20 @@ console.log('Explainer/Labs loaded')
 
 const Labs = ({}) => {
     return(<div className="content">
-    <p>Leave comments/suggestions for website copy <a href="https://docs.google.com/document/d/15qiNDE5isQwJukAH_OhxTcr21A5mVVkQCGsxIH3Bb7g/edit?tab=t.0">on this document here</a></p>
-    <h1>[Under construction] Welcome to BCU Labs!</h1>
+    <h1>[Under construction]</h1>
         <p>
-            
-            
-
+        Leave comments/suggestions for website copy <a 
+        href="https://docs.google.com/document/d/15qiNDE5isQwJukAH_OhxTcr21A5mVVkQCGsxIH3Bb7g/edit?tab=t.gq5m123bmtzd#heading=h.qsy6igs1zwnv">on this document here</a>
+        </p>
+    <h1>Welcome to the BCU Labs Bike Stress Map!</h1>
+        <p>
+        BCU Labs is working to build data-backed tools to help people identify the strengths and weaknesses of the biking 
+        network in Greater Boston. We aim to help provide the vocabulary to communicate your experience biking in Boston. 
+        <br /> <br />
+        The Bike Stress Map is our first project, rating every street in Boston, Cambridge, Somerville, and Brookline based 
+        on how comfortable it is to ride a bike on. Start clicking around the map below to see your neighborhood or commute. 
+        Maybe you will find a new, more comfortable route. Keep scrolling to read up in more detail how we are rating streets 
+        and what you can do to improve the map.
         </p>
         
 </div>)}
