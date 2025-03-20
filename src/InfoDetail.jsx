c48ec99d75758c6e66d23d5d347ceff87e607f9d
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2025-03-20 00:05:43 -0400

    first pass of prep for data change

--- src/InfoDetail.jsx
+++ src/InfoDetail.jsx
@@ -9,27 +9,18 @@ const InfoDetail = ({selectedFeature}) => {
     const {
       name,
       highway,
-      LTS_left,
-      LTS_right,
-      biking_permitted_left,
-      biking_permitted_right,
-      biking_permitted_rule_left,
-      biking_permitted_rule_right,
+      LTS_fwd, LTS_rev,
+      bike_allowed_fwd, bike_allowed_rev,
       osmid,
       oneway,
       street_narrow_wide,
       width_street,
       width_street_rule,
-      width_parking_left,
-      width_parking_rule_left,
-      width_parking_right,
-      width_parking_rule_right,
+      width_parking_rev,
+      width_parking_fwd,
       lane_count,
       lane_count_rule,
-      parking_left,
-      parking_rule_left,
-      parking_right,
-      parking_rule_right,
+      parking_fwd, parking_rev,
       speed,
       speed_rule,
       centerline,
@@ -44,67 +35,60 @@ const InfoDetail = ({selectedFeature}) => {
     const osmidurl = "https://www.openstreetmap.org/way/" + osmid.toString()
 
     const BikeLaneInfo = ({data}) => {
-        const {highway, bike_lane_exist_left, bike_lane_exist_right, 
-            bike_lane_exist_rule_left, bike_lane_exist_rule_right,
-            bike_lane_separation_left, bike_lane_separation_right,
-            bike_lane_separation_rule_left, bike_lane_separation_rule_right,
-            width_bikelane_left, width_bikelane_right,
-            width_bikelane_rule_left, width_bikelane_rule_right,
-            width_bikelanebuffer_left, width_bikelanebuffer_right,
-            width_bikelanebuffer_rule_left, width_bikelanebuffer_rule_right,
-            bikelane_reach_left, bikelane_reach_right} = data
-        console.log('bike_lane_exist_left:', bike_lane_exist_left, 'bike_lane_exist_right:', bike_lane_exist_right)
-        // console.log(bike_lane_exist_left == 'no', bike_lane_exist_right == 'no', bike_lane_exist_left == 'no' && bike_lane_exist_right == 'no')
+        const {highway, bike_lane_fwd, bike_lane_rev, 
+            separation_fwd, separation_rev,
+            bike_width_fwd, bike_width_rev,
+            bike_width_rule_fwd, bike_width_rule_rev,
+            buffer_fwd, buffer_rev,
+            buffer_rule_fwd, buffer_rule_rev,
+            bike_reach_fwd, bike_reach_rev} = data
+        console.log('bike_lane_fwd:', bike_lane_fwd, 'bike_lane_rev:', bike_lane_rev)
 
         let bikelane = ''
 
-        if (!(bike_lane_exist_left != 'no' || bike_lane_exist_right != 'no') && (highway == 'footway' || highway == 'cycleway' || highway == 'service')) {
+        if (!(bike_lane_fwd != 'no' || bike_lane_rev != 'no') && (highway == 'footway' || highway == 'cycleway' || highway == 'service')) {
             return (<></>)
         } else {
             bikelane = <>
             <tr><td className='tableDescription' colSpan="2">Bike Lane</td></tr>
             <tr>
-                <td className='tableValue'>{bike_lane_exist_left}</td>
-                <td className='tableValue'>{bike_lane_exist_right}</td>
-            </tr>
-            <tr>
-                <td className='notes'>{bike_lane_exist_rule_left}</td>
-                <td className='notes'>{bike_lane_exist_rule_right}</td>
+                <td className='tableValue'>{bike_lane_rev}</td>
+                <td className='tableValue'>{bike_lane_fwd}</td>
             </tr>
             </>
         }
 
-        if (bike_lane_exist_left == 'no' && bike_lane_exist_right == 'no' && highway != 'footway') return (bikelane)
+        if (bike_lane_fwd == 'no' && bike_lane_rev == 'no' && highway != 'footway') return (bikelane)
         
         return (
             <>
             {bikelane}
             <tr>
-                <td className='notes'>{bike_lane_separation_rule_left}</td>
-                <td className='notes'>{bike_lane_separation_rule_right}</td>
+                <td className='notes'>{separation_rev}</td>
+                <td className='notes'>{separation_fwd}</td>
             </tr>
             <tr><td className='tableDescription' colSpan="2">Bike Lane Width</td></tr>
             <tr>
-                <td className='tableValue'>{Number(width_bikelane_left).toFixed(1)}</td>
-                <td className='tableValue'>{Number(width_bikelane_right).toFixed(1)}</td>
+                <td className='tableValue'>{Number(bike_width_rev).toFixed(1)}</td>
+                <td className='tableValue'>{Number(bike_width_fwd).toFixed(1)}</td>
             </tr>
             <tr>
-                <td className='notes'>{width_bikelane_rule_left}</td>
-                <td className='notes'>{width_bikelane_rule_right}</td>
+                <td className='notes'>{bike_width_rule_rev}</td>
+                <td className='notes'>{bike_width_rule_fwd}</td>
             </tr>
             <tr><td className='tableDescription' colSpan="2">Bike Lane Buffer</td></tr>
             <tr>
-                <td className='tableValue'>{Number(width_bikelanebuffer_left).toFixed(1)}</td>
-                <td className='tableValue'>{Number(width_bikelanebuffer_right).toFixed(1)}</td>
+                <td className='tableValue'>{Number(buffer_rev).toFixed(1)}</td>
+                <td className='tableValue'>{Number(buffer_fwd).toFixed(1)}</td>
             </tr>
             <tr>
-                <td className='notes'>{width_bikelanebuffer_rule_left}</td>
-                <td className='notes'>{width_bikelanebuffer_rule_right}</td>
+                <td className='notes'>{buffer_rule_rev}</td>
+                <td className='notes'>{buffer_rule_fwd}</td>
             </tr>
             <tr><td className='tableDescription' colSpan="2">Bike Lane Reach</td></tr>
             <tr>
-                <td className='tableValue'>{Number(bikelane_reach_left).toFixed(1)}</td>
-                <td className='tableValue'>{Number(bikelane_reach_right).toFixed(1)}</td>
+                <td className='tableValue'>{Number(bike_reach_rev).toFixed(1)}</td>
+                <td className='tableValue'>{Number(bike_reach_fwd).toFixed(1)}</td>
             </tr>
             </>
         )
@@ -112,21 +96,19 @@ const InfoDetail = ({selectedFeature}) => {
 
     const LTSInfo = ({data}) => {
         const {
-            LTS_biking_permitted_left, LTS_biking_permitted_right,
-            LTS_bike_lane_separation_left, LTS_bike_lane_separation_right,
-            LTS_mixed_left, LTS_mixed_right,
-            LTS_bikelane_noparking_left, LTS_bikelane_noparking_right, 
-            LTS_bikelane_yesparking_left, LTS_bikelane_yesparking_right
+            LTS_bike_access_fwd, LTS_bike_access_rev,
+            LTS_mixed_fwd, LTS_mixed_rev,
+            LTS_bikelane_noparking_rev, LTS_bikelane_noparking_fwd, 
+            LTS_bikelane_yesparking_rev, LTS_bikelane_yesparking_fwd
         } = data
 
-        const biking_permitted = ['Biking Permitted', LTS_biking_permitted_left, LTS_biking_permitted_right]
-        const bike_lane_separation = ['Bike Lane Separation', LTS_bike_lane_separation_left, LTS_bike_lane_separation_right]
-        const mixed = ['Mixed Traffic', LTS_mixed_left, LTS_mixed_right]
-        const bikelane_noparking = ['Bike lane without street parking', LTS_bikelane_noparking_left, LTS_bikelane_noparking_right]
-        const bikelane_yesparking = ['Bike lane with street parking', LTS_bikelane_yesparking_left, LTS_bikelane_yesparking_right]
+        const bike_access = ['Biking Access', LTS_bike_access_rev, LTS_bike_access_fwd]
+        const mixed = ['Mixed Traffic', LTS_mixed_rev, LTS_mixed_fwd]
+        const bikelane_noparking = ['Bike lane without street parking', LTS_bikelane_noparking_rev, LTS_bikelane_noparking_fwd]
+        const bikelane_yesparking = ['Bike lane with street parking', LTS_bikelane_yesparking_rev, LTS_bikelane_yesparking_fwd]
 
         let ltsRow = []
-        for (const ltsType of [mixed, biking_permitted, bike_lane_separation, bikelane_noparking, bikelane_yesparking]) {
+        for (const ltsType of [mixed, bike_access, bikelane_noparking, bikelane_yesparking]) {
             console.log(ltsType[0], (typeof ltsType[1] !== 'undefined'), (typeof ltsType[2] !== 'undefined'))
             if ((typeof ltsType[1] !== 'undefined') || (typeof ltsType[2] !== 'undefined')) {
                 ltsRow.push([<>
@@ -153,25 +135,16 @@ const InfoDetail = ({selectedFeature}) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr><td className='tableDescription' colSpan="2">OSM Side</td></tr>
-                        <tr>
-                            <th className='tableValue'>Left</th>
-                            <th className='tableValue'>Right</th>
-                        </tr>
                         <tr><td className='tableDescription' colSpan="2">LTS</td></tr>
                         <tr>
-                            <td className='tableValue'><b>{LTS_left}</b></td>
-                            <td className='tableValue'><b>{LTS_right}</b></td>
+                            <td className='tableValue'><b>{LTS_rev}</b></td>
+                            <td className='tableValue'><b>{LTS_fwd}</b></td>
                         </tr>
                         <tr><td className='tableSection' colSpan="2">Bike Infrastructure</td></tr>
                         <tr><td className='tableDescription' colSpan="2">Bike Permitted</td></tr>
                         <tr>
-                            <td className='tableValue'>{biking_permitted_left}</td>
-                            <td className='tableValue'>{biking_permitted_right}</td>
-                        </tr>
-                        <tr>
-                            <td className='notes'>{biking_permitted_rule_left}</td>
-                            <td className='notes'>{biking_permitted_rule_right}</td>
+                            <td className='tableValue'>{bike_allowed_rev}</td>
+                            <td className='tableValue'>{bike_allowed_fwd}</td>
                         </tr>
                         <BikeLaneInfo data = {selectedFeature.properties}/>
 
@@ -190,12 +163,8 @@ const InfoDetail = ({selectedFeature}) => {
 
                         <tr><td className='tableDescription' colSpan="2">Parking Width</td></tr>
                         <tr>
-                            <td className='tableValue'>{Number(width_parking_left).toFixed(1)}</td>
-                            <td className='tableValue'>{Number(width_parking_right).toFixed(1)}</td>
-                        </tr>
-                        <tr>
-                            <td className='notes'>{width_parking_rule_left}</td>
-                            <td className='notes'>{width_parking_rule_right}</td>
+                            <td className='tableValue'>{Number(width_parking_rev).toFixed(1)}</td>
+                            <td className='tableValue'>{Number(width_parking_fwd).toFixed(1)}</td>
                         </tr>
                         
                         <tr><td className='tableDescription' colSpan="2">Lane Count</td></tr>
@@ -204,12 +173,8 @@ const InfoDetail = ({selectedFeature}) => {
 
                         <tr><td className='tableDescription' colSpan="2">Parking</td></tr>
                         <tr>
-                            <td className='tableValue'>{parking_left}</td>
-                            <td className='tableValue'>{parking_right}</td>
-                        </tr>
-                        <tr>
-                            <td className='notes'>{parking_rule_left}</td>
-                            <td className='notes'>{parking_rule_right}</td>
+                            <td className='tableValue'>{parking_rev}</td>
+                            <td className='tableValue'>{parking_fwd}</td>
                         </tr>
 
                         <tr><td className='tableDescription' colSpan="2">Prevailing Speed</td></tr>
