5f05f7363e3ea242e04f3047f49848b0f164f780
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2025-04-03 21:30:12 -0400

    fixed bearing calculation

--- src/InfoSimple.jsx
+++ src/InfoSimple.jsx
@@ -3,6 +3,16 @@
 console.log('InfoSimple loaded')
 
 export const CardinalDirection = ({start, end, inverse=false}) => {
+    // Converts from degrees to radians.
+    function toRadians(degrees) {
+        return degrees * Math.PI / 180;
+    };
+    
+    // Converts from radians to degrees.
+    function toDegrees(radians) {
+        return radians * 180 / Math.PI;
+    }
+  
 
     // console.log('typeof(start[0]):', typeof(start[0]))
     // console.log("typeof(start[0] === 'number')", typeof(start[0]) === 'number')
@@ -27,10 +37,23 @@ export const CardinalDirection = ({start, end, inverse=false}) => {
     // let lonnet = end[0] - start[0]
     // let latnet = end[1] - start[1]
 
-    let lonnet = lonend - lonstart
-    let latnet = latend - latstart
+    // let lonnet = lonend - lonstart
+    // let latnet = latend - latstart
 
-    let angle = Math.atan(latnet / lonnet) * (180 / Math.PI) // deg
+    // console.log('start:', lonstart, latstart)
+    // console.log('end:', lonend, latend)
+
+    latstart = toRadians(latstart);
+    lonstart = toRadians(lonstart);
+    latend = toRadians(latend);
+    lonend = toRadians(lonend);
+
+    let y = Math.sin(lonend - lonstart) * Math.cos(latend);
+    let x = Math.cos(latstart) * Math.sin(latend) - Math.sin(latstart) * Math.cos(latend) * Math.cos(lonend - lonstart);
+    let brng = Math.atan2(y, x);
+    let angle = (toDegrees(brng) + 360) % 360;
+
+    // let angle = Math.atan(latnet / lonnet) * (180 / Math.PI) // deg
 
     let cd = Math.floor((angle + 22.5) / 45)
 
@@ -40,7 +63,7 @@ export const CardinalDirection = ({start, end, inverse=false}) => {
         case 0:
         case 8:
         case -8:
-            cd = 'East'
+            cd = 'North'
             break;
         case 1:
         case -7:
@@ -48,15 +71,15 @@ export const CardinalDirection = ({start, end, inverse=false}) => {
             break;
         case 2:
         case -6:
-            cd = 'North'
+            cd = 'East'
             break;
         case 3:
         case -5:
-            cd = 'NorthWest'
+            cd = 'SouthEast'
             break;
         case 4:
         case -4:
-            cd = 'West'
+            cd = 'South'
             break;
         case 5:
         case -3:
@@ -64,19 +87,24 @@ export const CardinalDirection = ({start, end, inverse=false}) => {
             break;
         case 6:
         case -2:
-            cd = 'South'
+            cd = 'West'
             break;
         case 7:
         case -1:
-            cd = 'SouthEast'
+            cd = 'NorthWest'
             break;
     }
 
     // let output = angle + ' deg | CD: ' + cd
-    console.log('start:', lonstart, latstart)
-    console.log('end:', lonend, latend)
-    console.log('angle:', angle, ' | cd:', cd, ' | inverse:', inverse)
-    console.log('net: ', lonnet, latnet)
+    // let dellat = latend-latstart
+    // let dellon = lonend-lonstart
+    // let delnorm = Math.sqrt(dellat**2 + dellon**2)
+
+    // console.log('start:', lonstart, latstart)
+    // console.log('end:', lonend, latend)
+    // console.log('delta (lon/lat):', dellon/delnorm, dellat/delnorm)
+    // console.log('angle:', angle, ' | cd:', cd, ' | inverse:', inverse)
+    // console.log('net: ', lonnet, latnet)
     return cd
     }
 
