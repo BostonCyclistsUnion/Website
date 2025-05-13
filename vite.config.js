cb7732f236ea33d02a7a9880e9dee347122dfde6
Matthew Tschiegg <tschieggm@gmail.com>
2025-05-13 03:02:23 +0000

    Update the base URL path now that we have a BCU subdomain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://bostoncyclistsunion.github.io/Website/',
+  base: 'https://https://labs.bostoncyclistsunion.org/',
   plugins: [react()],
 })
