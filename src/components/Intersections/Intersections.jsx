d1a38167a81da275a9327382d7268e9fbb2546bd
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2025-07-04 10:20:12 -0400

    success returning geojson

--- src/components/Intersections/Intersections.jsx
+++ src/components/Intersections/Intersections.jsx
@@ -4,36 +4,22 @@ export default async function Intersections() {
     console.log('Fetching intersection data from google');
     let csvurl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT-8X_V7FUnDhcID9UGouljx_X0rMg5PAdM7lLwl1PmKXlB43sVIsurplO3qnjN3OE3_YEar4qUZzm7/pub?output=csv'
 
-    // async function fetchCSV(url) {
-    //     try {
-    //         const response = await fetch(url);
-    //         const data = await response.text();
-    //         console.log('CSV retrieved')
-    //         console.log(data)
-    //         return data
-    //     } catch (error) {
-    //         console.error('Error fetching CSV:', error);
-    //     }
-    //     }
-
-    // let csvdata = fetchCSV(csvurl);
-
     const response = await fetch(csvurl);
     const csvdata = await response.text();
-    console.log('CSV retrieved')
-    // console.log(csvdata)
+    console.log('CSV retrieved', await csvdata)
 
+    let geojsontest
+    
     csv2geojson.csv2geojson(await csvdata, {
         latfield: 'lat',
         lonfield: 'long',
         delimiter: ','
         }, function(err, data) {
-            console.log('csv2geojson function', err, data)
-            return data
+            // console.log('csv2geojson function', err, data)
+            geojsontest = data
+            return geojsontest
     }); 
+    // console.log('geojsontest from csv', await geojsontest)
 
-    // console.log('geojson from csv:')
-    // console.log(geojson)
-
-    // return geojson
+    return await geojsontest
 }
